03a7c718ad7800d4a644cf822a9fbf19cdc24481
Franco <franconlujan95@gmail.com>
2023-03-01 20:30:31 -0300

    Se agrega el proyecto

---
+++ funcion.js
@@ -0,0 +1,44 @@
+function mostrar(){
+    document.getElementById("menu1").style.display= "block";
+    
+}
+
+function mostrar2(){
+    document.getElementById("job1").style.display = "block";
+    
+}
+
+function mostrar3(){
+    document.getElementById("job2").style.display = "block";
+    
+}
+
+function mostrar4(){
+    document.getElementById("job3").style.display = "block";
+    
+}
+
+function mostrar5(){
+    document.getElementById("menu2").style.display = "block";
+    
+}
+
+function mostrar6(){
+    document.getElementById("infoPers").style.display = "block";
+    
+}
+
+function mostrar7(){
+    document.getElementById("infoCont").style.display = "block";
+    document.getElementById("infoCont2").style.display = "block";
+}
+
+function mostrar8(){
+    document.getElementById("menu3").style.display = "block";
+    
+}
+
+function mostrar9(){
+    document.getElementById("infoUni").style.display = "block";
+    
+}
